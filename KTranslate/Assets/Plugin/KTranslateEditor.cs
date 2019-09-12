3ac143080c86bbb1a7f4d51d628077c7030a0b57
PhiKirax <alexandre@richet.me>
2019-09-12 14:03:20 +0200

    correction data

---
+++ KTranslate/Assets/Plugin/KTranslateEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor;
+using UnityEditor.UIElements;
+
+namespace KTranslate {
+    [CustomEditor(typeof(KTranslateComponent))]
+    public class KTranslateEditor : Editor {
+        [MenuItem("Tools/KTranslate")]
+        private void OnEnable() {
+
+        }
+    }
+}
