16725781732f72d660158ff10f178f718d39f9e5
PhiKirax <alexandre@richet.me>
2019-09-13 17:34:19 +0200

    Add Asset file target

--- KTranslate/Assets/Editor/KTranslateEditor.cs
+++ KTranslate/Assets/Editor/KTranslateEditor.cs
@@ -8,6 +8,10 @@ namespace KTranslate {
     public class KTranslateEditor : EditorWindow {
         bool _generator;
         bool _loadLanguage;
+        bool _keyMenu;
+        int _choiceAlertGenerator = -1;
+        UnityEngine.Object _textTranslate;
+        TextAsset dictionnary;
         SystemLanguage _selectedLanguage, systemLanguageP;
 
         [MenuItem("Tools/KTranslater")]
@@ -18,16 +22,38 @@ namespace KTranslate {
         }
 
         void OnGUI() {
+            _textTranslate = EditorGUILayout.ObjectField(_textTranslate, typeof(TextAsset), true);
+            _keyMenu = EditorGUILayout.BeginFoldoutHeaderGroup(_keyMenu, "List Key");
+            if (_keyMenu) {
+                _selectedLanguage = (SystemLanguage)EditorGUILayout.EnumPopup("Language", _selectedLanguage);
+            }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+
             _generator = GUILayout.Button("Generate Original File");
-            _selectedLanguage = (SystemLanguage)EditorGUILayout.EnumPopup("Language", _selectedLanguage);
         }
 
         private void Update() {
             if (_generator) {
+                _choiceAlertGenerator = EditorUtility.DisplayDialogComplex("KTranslate new dictionary", 
+                    "Are you sure you want to generate a new file,\n\nIf there is already one it will be erased"
+                    , "Create / Replace", "Do nothing", "");
                 TaptTap();
             }
             if (_loadLanguage) {
 
+            }
+            if (_choiceAlertGenerator > -1) {
+                switch (_choiceAlertGenerator) {
+                    case 0:
+                        TaptTap();
+                        break;
+                    default:
+                        break;
+                }
             }
         }
 
