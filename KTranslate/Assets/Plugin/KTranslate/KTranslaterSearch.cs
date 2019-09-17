c03ac56ac56a8075cfd909e9436bd2791f0852ff
PhiKirax <alexandre@richet.me>
2019-09-17 11:38:01 +0200

    add apply scripts

---
+++ KTranslate/Assets/Plugin/KTranslate/KTranslaterSearch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KTranslaterSearch : MonoBehaviour {
+    private void Awake() {
+    }
+    void Start() {
+        var targetText = this.GetComponent<Text>();
+
+        targetText.text = KTranslate.KTranslaterApplyer.GetString(targetText.text);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
